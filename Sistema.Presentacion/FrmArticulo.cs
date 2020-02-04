ddc7e93b0bef49ee960a0ba9ea3cf919740baf93
Jose Luis Suller Aburto <j_suller@hotmail.com>
2020-02-04 08:45:10 -0500

    Agregar funcionalidad al formulario de articulo

--- Sistema.Presentacion/FrmArticulo.cs
+++ Sistema.Presentacion/FrmArticulo.cs
@@ -7,14 +7,131 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sistema.Negocio;
 
 namespace Sistema.Presentacion
 {
     public partial class FrmArticulo : Form
     {
+        private string RutaOrigen;
+        private string RutaDestino;
+        private string Directorio = "D:\\Proyectos\\SisVentas\\Imagenes\\";
         public FrmArticulo()
         {
             InitializeComponent();
+        }
+
+        private void Listar()
+        {
+            try
+            {
+                DgvListado.DataSource = NArticulo.Listar();
+                this.Formato();
+                this.Limpiar();
+                LblTotal.Text = "Total registros: " + Convert.ToString(DgvListado.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void Buscar()
+        {
+            try
+            {
+                DgvListado.DataSource = NArticulo.Buscar(TxtBuscar.Text);
+                this.Formato();
+                LblTotal.Text = "Total registros: " + Convert.ToString(DgvListado.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void Formato()
+        {
+            DgvListado.Columns[0].Visible = false;
+            DgvListado.Columns[0].Width = 100;
+            DgvListado.Columns[1].Width = 50;
+            DgvListado.Columns[2].Visible = false;
+            DgvListado.Columns[3].Width = 100;
+            DgvListado.Columns[3].HeaderText = "Categoría";
+            DgvListado.Columns[4].Width = 100;
+            DgvListado.Columns[4].HeaderText = "Código";
+            DgvListado.Columns[5].Width = 150;
+            DgvListado.Columns[6].Width = 100;
+            DgvListado.Columns[6].HeaderText = "Precio Venta";
+            DgvListado.Columns[7].Width = 60;
+            DgvListado.Columns[8].Width = 200;
+            DgvListado.Columns[8].HeaderText = "Descripción";
+            DgvListado.Columns[9].Width = 100;
+            DgvListado.Columns[10].Width = 100;
+        }
+
+        private void Limpiar()
+        {
+            TxtBuscar.Clear();
+            TxtNombre.Clear();
+            TxtId.Clear();
+            TxtDescripcion.Clear();
+            BtnInsertar.Visible = true;
+            ErrorIcono.Clear();
+            BtnActualizar.Visible = false;
+
+            DgvListado.Columns[0].Visible = false;
+            BtnActivar.Visible = false;
+            BtnDesactivar.Visible = false;
+            BtnEliminar.Visible = false;
+            ChkSeleccionar.Checked = false;
+        }
+
+        private void MensajeError(string Mensaje)
+        {
+            MessageBox.Show(Mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void MensajeOk(string Mensaje)
+        {
+            MessageBox.Show(Mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void CargarCategoria()
+        {
+            try
+            {
+                CboCategoria.DataSource = NCategoria.Seleccionar();
+                CboCategoria.ValueMember = "idcategoria";
+                CboCategoria.DisplayMember = "nombre";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void FrmArticulo_Load(object sender, EventArgs e)
+        {
+            this.Listar();
+            this.CargarCategoria();
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            this.Buscar();
+        }
+
+        private void BtnCargarImagen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg, *.jpeg, *.jpe, *.jfif, *.png";
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                PicImagen.Image = Image.FromFile(file.FileName);
+                TxtImagen.Text = file.FileName.Substring(file.FileName.LastIndexOf("\\")+1);
+                this.RutaOrigen = file.FileName;
+            }
         }
     }
 }
